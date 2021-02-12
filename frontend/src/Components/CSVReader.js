c4f71fa4c9f43cbf5b601b1cab98415b9e49a1b1
Vili Tulonen <tulonenvili@gmail.com>
2021-02-12 17:03:02 +0200

    update csvreader config options

--- frontend/src/Components/CSVReader.js
+++ frontend/src/Components/CSVReader.js
@@ -4,6 +4,10 @@ import { CSVReader } from 'react-papaparse'
 export default class CSVReaderDragAndDrop extends Component {
   handleOnDrop = (data) => {
     console.log(data)
+    console.log(data[0])
+    console.log(data[0].data)
+    console.log(data[0].data.Date)
+    
   }
 
   handleOnError = (err, file, inputElem, reason) => {
@@ -24,7 +28,10 @@ export default class CSVReaderDragAndDrop extends Component {
           addRemoveButton
           onRemoveFile={this.handleOnRemoveFile}
           config={{
-            transform: (value) => value.trim(),
+            header: true,
+            //Transform keys and values to strings w/o whitespace
+            transformHeader: (value) => value.trim().replace('/', '').toLowerCase(),
+            transform: (value) => value.trim().replace('$', ''),
           }}
         >
           <span>Drop CSV file here or click to upload.</span>
