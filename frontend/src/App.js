93cd3a0f8b2f6f1d810337a506c645e4817e2d87
Vili Tulonen <tulonenvili@gmail.com>
2021-02-21 13:22:01 +0200

    add dateRangePicker, buttons to test submitting csv and log data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CSVReader from './Components/CSVReader'
+import DateRangePicker from '@wojtekmaj/react-daterange-picker'
+import axios from 'axios'
 
 const App = () => {
-  return <div>
-    <CSVReader />
-  </div>
+  const [dateRange, setDateRange] = useState([new Date(), new Date()])
+  const [startDate, setStartDate] = useState(null)
+  const [endDate, setEndDate] = useState(null)
+  const [documentID, setDocumentID] = useState("")
+  const [quotes, setQuotes] = useState([])
+
+  const handleUpload = async (data) => {
+    // TODO: validate csv
+    // if valid do:
+    const quotesArray = data.map((item) => item.data)
+    setQuotes(quotesArray)
+  }
+
+  const handleDateChange = (dates) => {
+    console.log('dates', dates)
+    setDateRange(dates)
+    setStartDate(dates[0].toISOString().split('T')[0])
+    setEndDate(dates[1].toISOString().split('T')[0])
+  }
+
+  const submitCSV = async (e) => {
+    console.log(e)
+    const stockData = {
+      code: "test",
+      quotes: []
+    }
+    // post csv data
+    const csvResponse = await axios.post('/api/csv', stockData)
+    setDocumentID(csvResponse.data.id)
+
+    // update quotes to include csv id
+    const updatedQuotes = quotes.map(q => ({...q, importedCSV: csvResponse.data.id}))
+    
+    // post quotes
+    const quotesResponse = await axios.post('/api/quotes', {quotes: updatedQuotes, csvId: csvResponse.data.id})
+    console.log(quotesResponse)
+  } 
+
+  return (
+    <div>
+      <CSVReader handleUpload={handleUpload} />
+      <DateRangePicker onChange={handleDateChange} value={dateRange} />
+      <button
+        onClick={() => {
+          console.log(documentID)
+          console.log(quotes)
+          console.log('range', dateRange)
+          console.log('start', startDate)
+          console.log('end', endDate)
+        }}
+      >
+        log
+      </button>
+  
+      <button
+        onClick={async () => {
+          const response = await axios.get(
+            `/api/quotes/csv/${documentID}/bullish/?start=${startDate}&end=${endDate}`
+          )
+          const days = response.data.days
+          const start = new Date(response.data.between[0])
+          const end = new Date(response.data.between[1])
+          console.log(response.data)
+          console.log(`Longest bullish trend: ${days} days between ${start.toDateString()} and ${end.toDateString()}`)
+          return response.data
+        }}
+      >
+        bullish
+      </button>
+      <button
+        onClick={submitCSV}>
+        submit
+      </button>
+    </div>
+  )
 }
 
 export default App
--- frontend/src/Components/CSVReader.js
+++ frontend/src/Components/CSVReader.js
@@ -1,22 +1,8 @@
 import React, { Component } from 'react'
 import { CSVReader } from 'react-papaparse'
-import axios from 'axios'
 
 export default class CSVReaderDragAndDrop extends Component {
-  handleOnDrop = async (data) => {
-    // TODO: validate csv
-    // if valid do:
-    const quoteArray = data.map((item) => item.data)
-    const stockData = {
-      code: 'testcode', // change to user input later
-      quotes: quoteArray,
-    }
-    
-    console.log(stockData)
-    const response = await axios.post('/api/stockData', stockData)
-    return response.data
-  }
-
+ 
   handleOnError = (err, file, inputElem, reason) => {
     console.log(err)
   }
@@ -30,11 +16,12 @@ export default class CSVReaderDragAndDrop extends Component {
       <>
         <h5>Click and Drag Upload</h5>
         <CSVReader
-          onDrop={this.handleOnDrop}
+          onDrop={this.props.handleUpload}
           onError={this.handleOnError}
           addRemoveButton
           onRemoveFile={this.handleOnRemoveFile}
           config={{
+            skipEmptyLines: true,
             header: true,
             //Transform keys and values to strings w/o whitespace
             transformHeader: (value) =>
